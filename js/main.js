93fbbf329e2551d9d533bd79ba51100ae0ca8812
Mostafa Yassin <mostafayassin292@gmail.com>
2025-09-25 18:11:28 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+/* 
+==  vs ===
+Lose Eqaulity Operator   [==]
+Strict Eqaulity Operator [===]
+
+
+compare between value    [==]
+compare between value and type [===]
+*/
+
+// if (10 === 10) {
+//   console.log("true")
+// } else {
+//   console.log("false")
+// }
+
+
+// console.log(10 + "10") // 1010 
+
+
+// for (;;) {
+
+// }
+
+// for( var i = 0 ; i < 3 ;  ) {
+// 	// Code  ===> infinity loop 
+// }
+
+// for(  ; ) {
+// 	// Code  ===> Syntax Error
+// }
+
+// undefined vs null
+// var username;
+// console.log(username)
+
+// var title = null;
+// console.log(title)
+
+// What Is Deffirent Between function declaration and expression
+
+
+
+// funName();
+// function funName() {
+//   console.log("hello world")
+// }
+
+
+// var printMsg = () => {
+//   console.log("message")
+// }
+// printMsg()
+
+
+// var a = 5;
+
+// (function(){
+//     console.log(a);
+//     var a = 10;
+// })();
+
+// function foo() {
+//     function bar() { return 3; }
+//     return bar();
+//     function bar() { return 8; }
+// }
+// console.log( foo() );
+
+
+// function foo() {
+//     var bar = function() { return 3; }
+//     return bar();
+//     var bar = function() { return 8; }
+// }
+// console.log( foo() );
+
+
+// function foo() {
+//     return bar();
+//     function bar() { return 3; }
+//     var bar = function() { return 8; }
+// }
+// console.log( foo() );
+
+// console.log( foo() );
+
+// var foo = function() {
+//     return bar();
+//     function bar() { return 3; }
+//     var bar = function() { return 8; }
+// }
+
+// var user = {
+//   wife: {
+//     son: {
+//       name: "Ahmed Ali",
+//     },
+//   },
+// };
+
+// console.log(user["wife"])
+// console.log(user["wife"]["son"])
+// console.log(user['wife']['son']['name'])
